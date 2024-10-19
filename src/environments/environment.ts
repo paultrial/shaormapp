cf1f9ca6e52c10a147e1d44d31f0af4c1296036e
p.olteanu <p.olteanu@rosoftlab.net>
2024-10-19 16:58:45 +0300

    stupid

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    url: 'https://shaormapp-paultrials-projects.vercel.app',
+    url: 'https://shaormapp.vercel.app',
     apiUrl: "http://127.0.0.1:8069"
 };
