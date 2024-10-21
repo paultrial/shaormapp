7efdcdcf4a98346e6f881857890cbc6b57a5545e
p.olteanu <p.olteanu@rosoftlab.net>
2024-10-21 09:20:47 +0300

    new web server

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     url: 'https://shaormapp.vercel.app',
-    apiUrl: "https://paultrial.pythonanywhere.com"
+    apiUrl: "https://web-uway6z9s5wwf.up-de-fra1-k8s-1.apps.run-on-seenode.com"
 };
